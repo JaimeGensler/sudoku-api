feeae04df4f4abe51c5d0faa3828f84558ee1290
Jaime Gensler <jaimegensler0@gmail.com>
2020-01-31 10:12:34 -0800

    Create Grid controller

---
+++ app/controllers/grids_controller.rb
@@ -0,0 +1,37 @@
+class GridsController < ApplicationController
+    def index
+        @grids = Grid.all
+        json_response(@grids)
+    end
+
+    def show
+        @grid = Grid.find(params[:id])
+        json_response(@grid)
+    end
+
+    def create
+        @grid = Grid.create!(grid_params)
+        json_response(@grid, 201)
+    end
+
+    def update
+        @grid = Grid.find(params[:id])
+        if @grid.update!(grid_params)
+            render status: 200, json: {
+                message: "This grid (#{@grid.name}) has been updated successfully."
+            }
+        end
+    end
+
+    def destroy
+        @grid = Grid.find(params[:id])
+        if @grid.destroy!
+            render status: 200, json: {message: "Destroyed grid: #{@grid.name}"}
+        end
+    end
+
+    private
+    def grid_params
+        params.permit(:solved_grid, :puzzle_grid, :difficulty)
+    end
+end
