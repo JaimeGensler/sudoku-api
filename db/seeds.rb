553cb9c2749c0af3ea7451e681536c0bd97d2ca5
Jaime Gensler <jaimegensler0@gmail.com>
2020-01-31 11:23:34 -0800

    Add database seeding, rspec config

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+    def self.start(count = 20)
+        puts '=== Started Seeding ==='
+        Grid.destroy_all
+        create_grids(count)
+        puts '=== Completed Seeding ==='
+    end
+
+    def self.create_grids(count)
+        count.times do
+            Grid.create!(
+                solved_grid: (rand(1..9).to_s * 81),
+                puzzle_grid: (rand(0...9).to_s * 81),
+                difficult: ['easy', 'medium', 'hard'].shuffle.first
+            )
+        end
+        puts "Created #{Grid.count} example grids."
+    end
+end
+
+Seed.start
+
+#these grids are NOT viable boards, and are only meant to be placeholders.
