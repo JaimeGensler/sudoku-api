4f02dd1fa21bb397207b8620d7dd63c3b6f3d2f9
Jaime Gensler <jaimegensler0@gmail.com>
2020-01-31 13:01:50 -0800

    Add tests for GET and POST

---
+++ spec/requests/get_grids_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe 'GET /grids', :type => :request do
+    it 'returns [all grids, status code 200]' do
+        get '/grids'
+
+        expect(JSON.parse(response.body).size).to eq 20
+        expect(response).to have_http_status 200
+    end
+end
---
+++ spec/requests/post_grids_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe 'POST /grids', :type => :request do
+    context '(valid request)' do
+        it 'returns [success message, status code 201]' do
+            post '/grids', params: {
+                solved_grid: "#{'2' * 81}",
+                puzzle_grid: "#{'0' * 81}",
+                difficulty: 'medium'
+            }
+
+            expect(JSON.parse(response.body)['id']).to be_an_instance_of(Integer)
+            expect(response).to have_http_status 201
+        end
+    end
+
+    context '(bad request)' do
+        it 'returns [error message, status code 422]' do
+            post '/grids', params: {
+                solved_grid: "0123456789",
+                puzzle_grid: "0105105010",
+                difficulty: 'medium'
+            }
+
+            expect(JSON.parse(response.body)['message']).to eq 'Validation failed: Puzzle grid is the wrong length (should be 81 characters), Solved grid is the wrong length (should be 81 characters)'
+        end
+    end
+end
